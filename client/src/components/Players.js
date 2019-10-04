317f11235bec5c370a36c65f5d5fca940a2c9d17
unknown <neduvinci@gmail.com>
2019-10-04 12:37:44 +0100

    Working on the SearchForm incorporating a custom hook

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Player from './Player';
+import SearchForm from './SearchForm';
+import useInput from '../hooks/useInput';
 
 export default function Players({ playersList }) {
+    const [country, setCountry] = useInput('');
+
     return (
         <>
             <h1>Players</h1>
+            <SearchForm />
             {playersList.map(player => 
                 <Player key={player.id} player={player}/>)}
         </>
--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 export default function SearchForm(props) {
+
+    const { } = props;
     return (
-        <form action="">
-            <label htmlFor="">
-                <input type="text"/>
+        <form>
+            <div>
+                <label htmlFor="countryInput">Country Search</label>
+                <input type="text" id='countryInput' name='country' placeholder='Country' />
                 <button>Search</button>
-            </label>
-            <label htmlFor="">
-                <input type="text"/>
+            </div>
+            <div>
+                <label htmlFor="popularityInput">Popularity Search</label>
+                <input type="text" id='popularityInput' name='popularity' placeholder='high/low' />
                 <button>Search</button>
-            </label>
+            </div>
+
         </form>
     )
 }
