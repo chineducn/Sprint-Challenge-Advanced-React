6feae3af1d7e418983da6434d81a154bd7686cda
unknown <neduvinci@gmail.com>
2019-10-04 09:21:33 +0100

    Started creating the SearchForm component

---
+++ client/src/components/SearchForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function SearchForm(props) {
+    return (
+        <form action="">
+            <label htmlFor="">
+                <input type="text"/>
+                <button>Search</button>
+            </label>
+            <label htmlFor="">
+                <input type="text"/>
+                <button>Search</button>
+            </label>
+        </form>
+    )
+}
