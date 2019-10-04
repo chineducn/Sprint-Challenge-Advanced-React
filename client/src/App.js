f0230608de733f0aee0782d449a96be5e8954bdc
unknown <neduvinci@gmail.com>
2019-10-04 08:55:24 +0100

    Added state variables in App, retrieved data from api using axios and passed state to Players component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import Players from './components/Players';
+import axios from 'axios';
 
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      playersList: [],
+      searchresult: [],
+      searchInput: '',
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players')
+      .then(response => {
+        this.setState({
+          playersList: response.data,
+          searchresult: response.data,
+        })
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <Players />        
+        <Players playersList={this.state.playersList}/>        
       </div>
     );
   }
--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function Player() {
+export default function Player({ playersList }) {
     return (
         <>
             <h1>Players</h1>
+            {playersList.map(player => 
+            <h2 key={player.id}>{player.name}</h2>)}
         </>
     )
 };
